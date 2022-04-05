2b1f8c0728d6fa740621c8a64c15355937676583
Lula <lula@42sp.org.br>
2022-04-05 19:41:06 -0300

    regras do desafio nos comments do testes_palavra.py

--- tdd_palavra/testes/testes_palavra.py
+++ tdd_palavra/testes/testes_palavra.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 import negocio.palavra as negocio
 
+# Se a letra está no lugar certo ✅
+# Se a letra está no lugar errado ⚠️
+# Se a letra não existe na palavra ❌
+
 class TesteDeExemplo(TestCase):
 
     def teste_de_exemplo(self):
