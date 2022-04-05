28406a030d99a0f6ca1971bbc2b2d3b04fab713d
Lula <lula@42sp.org.br>
2022-04-05 19:44:08 -0300

    tirando emojis do enunciado

--- tdd_palavra/testes/testes_palavra.py
+++ tdd_palavra/testes/testes_palavra.py
@@ -1,13 +1,13 @@
 from unittest import TestCase
 import negocio.palavra as negocio
 
-# Se a letra está no lugar certo ✅
-# Se a letra está no lugar errado ⚠️
-# Se a letra não existe na palavra ❌
+# Se a letra está no lugar certo *
+# Se a letra está no lugar errado -
+# Se a letra não existe na palavra X
 
 class TesteDeExemplo(TestCase):
 
-    def teste_de_exemplo(self):
+    def teste_palavra_lula_chute_doria_retorna_a(self):
         a = 20
         b = 25
         esperado = 45
