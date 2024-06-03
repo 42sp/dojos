67a191b549ae9e5a7bd5974ab6022c6939bbc544
adantas- <adantas-@student.42sp.org.br>
2024-06-03 18:08:03 -0300

    +: is working and i hope it isn't too hard

--- SQL/src/init.sql
+++ SQL/src/init.sql
@@ -1,5 +1,12 @@
 CREATE TABLE IF NOT EXISTS population (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255) NOT NULL
+	last_name VARCHAR(255) DEFAULT NULL
 );
+
+LOAD DATA INFILE '/var/lib/mysql-files/population.csv'
+INTO TABLE population
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(first_name, last_name);
--- SQL/src/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sleep 30
-python /app/populate.py
-mysqld
