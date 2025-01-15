e4ff250c6457d62ee08326444f4655cd0330c7c3
Everton <egeraldo@student.42sp.org.br>
2025-01-15 09:46:44 -0300

    atualizando o dojo

---
+++ docker-compose2/app/backend/something.py
@@ -0,0 +1,3 @@
+print("Esse é so um exemplo, vc pode usar a liguagem que quiser")
+print("Esse é so um exemplo, vc pode usar a liguagem que quiser")
+print("Esse é so um exemplo, vc pode usar a liguagem que quiser")
